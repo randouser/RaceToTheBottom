be2093c409b51208129818252daeb863242f6615
Xenogyst <xenogyst@gmail.com>
2014-07-23 01:40:01 -0500

    Fix for jackson parser not knowing what to do with abstract gamelog class.

--- Server/src/main/java/org/group3/game/model/game/GameLog.java
+++ Server/src/main/java/org/group3/game/model/game/GameLog.java
@@ -1,6 +1,7 @@
 package org.group3.game.model.game;
 
 
+import org.codehaus.jackson.annotate.JsonTypeInfo;
 import org.group3.game.model.card.Card;
 import org.group3.game.model.game.Player;
 import org.group3.game.model.game.District;
@@ -8,6 +9,7 @@ import org.group3.game.model.game.District;
 import java.util.List;
 import java.util.ArrayList;
 
+@JsonTypeInfo(use= JsonTypeInfo.Id.CLASS, include= JsonTypeInfo.As.PROPERTY, property="class")
 public abstract class GameLog {
 
     protected String logMessage;
