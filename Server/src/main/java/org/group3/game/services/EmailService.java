637beb04b1e8a889208ae9eee3902574dd44a093
randouser <ics499ep@gmail.com>
2014-07-26 16:13:14 -0500

    added half-assed routine to get host IP. Assumes eth0 is the outward facing interface. Adds :8080 if 192.168

--- Server/src/main/java/org/group3/game/services/EmailService.java
+++ Server/src/main/java/org/group3/game/services/EmailService.java
@@ -77,7 +77,7 @@ public class EmailService {
 		    	
 		    	SERVER_ADDRESS = "http://192.168.56.102:8080";
 		    	
-//		    	SERVER_ADDRESS = "http://www.cornerinternets.com:8080";
+//		    	SERVER_ADDRESS = "http:cornerinternets.com:8080";
 		    	
 		    	
 		    }
