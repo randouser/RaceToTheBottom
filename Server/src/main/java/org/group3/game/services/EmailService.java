e48544a082010a97383f86d10abf225a7dda6c78
randouser <ics499ep@gmail.com>
2014-08-07 22:13:01 -0400

    Refactored emailservice so we can just comment out or delete helper methods for local VM when we want to deploy final version. Final version will just set address to http://cornerinternets.com

--- Server/src/main/java/org/group3/game/services/EmailService.java
+++ Server/src/main/java/org/group3/game/services/EmailService.java
@@ -27,63 +27,27 @@ public class EmailService {
     private MailSender mailSender;
 
     private String SERVER_ADDRESS;
+    
+    //Can be removed for final version, assists with using local VM
+    //for development and testing.
+    private boolean isLocalVM;
 
     //TODO there probably is a better way to get the location of the server
     
+    
+    //Note: isLocalVM and setReturnPath can be removed for "final" version.
+    //They are in place only to aid with local testing and development on a VM.
+    //In final version we can just set the address directly to "cornerinternets.com"
     public EmailService(){
     
-	    InetAddress servIp;
-	    
-		
-	    try {
-	    	
-		    Enumeration<NetworkInterface> networkDevices = NetworkInterface.getNetworkInterfaces();
-		    
-		    while (networkDevices.hasMoreElements())
-		    {
-		    	
-		    	NetworkInterface ni = networkDevices.nextElement();
-		    	
-		    	if (ni.getDisplayName().equals("eth0")) {
-		    	
-		    		Enumeration<InetAddress> addies = ni.getInetAddresses();
-		    	
-		    		//InetAddress will have two elements, IP6 addy, IP4 addy, get IP4 addy
-		    		while (addies.hasMoreElements())
-		    		{
-		    			
-		    			SERVER_ADDRESS = addies.nextElement().getHostAddress();
-		    			
-		    		}
-		    		
-		    		if (SERVER_ADDRESS.contains("192.168"))
-		    		{
-		    			
-		    			SERVER_ADDRESS += ":8080";
-		    			
-		    		}
-		    		
-		    		break;
-		    	
-		    	}
-		    	
-		    	
-		    } 
-		    
-	    }catch (SocketException ex)
-		    {
-		    	
-		    	logger.info("Couldn't get server IP, defaulting...");
-		    	
-		    	SERVER_ADDRESS = "http://192.168.56.102:8080";
-		    	
-//		    	SERVER_ADDRESS = "http://cornerinternets.com";
-		    	
-		    	
-		    }
+    	isLocalVM();
+    	setReturnPath();
+    	
+    	//SERVER_ADDRESS = "cornerinternets.com";
 	    	
     	
     }
+    
 
     public void sendMail(String from, String to, String subject, String msg) {
         logger.info("Sending email to: " + to + "  from: " + from );
@@ -108,6 +72,86 @@ public class EmailService {
                 "Hello " + inviteeEmail + ",\n" + fromUser.getName() + " has started a game with RaceToTheBottom and would like you to join." +
                         "\nFollow this link to join:\n" +
                         SERVER_ADDRESS + "/#register?gameId=" + gameId + "&invitee=" + encodedEmail);
+    }
+    
+    //Can be removed for final version, assists with using local VM
+    //for development and testing.
+    private void isLocalVM()
+    {
+    	
+	    InetAddress servIp;
+	    
+	    String testForLocalVM = "";
+	    
+	    try {
+	    	
+		    Enumeration<NetworkInterface> networkDevices = NetworkInterface.getNetworkInterfaces();
+		    
+		    while (networkDevices.hasMoreElements())
+		    {
+		    	
+		    	NetworkInterface ni = networkDevices.nextElement();
+		    	
+		    	if (ni.getDisplayName().equals("eth0")) {
+		    	
+		    		Enumeration<InetAddress> addies = ni.getInetAddresses();
+		    	
+		    		//InetAddress will have two elements, IP6 addy, IP4 addy, get IP4 addy
+		    		while (addies.hasMoreElements())
+		    		{
+		    			
+		    			testForLocalVM = addies.nextElement().getHostAddress();
+		    			
+		    		}
+		    		
+		    		if (testForLocalVM.contains("192.168"))
+		    		{
+		    			
+		    			testForLocalVM += ":8080";
+		    			
+		    			String prependHTTP = "http://";
+		    			
+		    			prependHTTP += testForLocalVM;
+		    			
+		    			this.SERVER_ADDRESS = prependHTTP;
+		    			
+		    			this.isLocalVM = true;
+		    			
+		    			return;
+		    			
+		    		}
+		    	
+		    	}
+		    	
+		    	
+		    } 
+		    
+	    }catch (SocketException ex)
+		    {
+		    	
+		    	this.isLocalVM = false;
+		    	return;
+		    	
+		    }
+    	
+	   this.isLocalVM = false;
+	   return;
+	    
+    	
+    }
+    
+    //Can be removed for final version, assists with using local VM
+    //for development and testing.
+    private void setReturnPath ()
+    {
+    	
+    	if (!isLocalVM)
+    	{
+    		
+    		this.SERVER_ADDRESS = "http://cornerinternets.com";
+    		
+    	}
+    	
     }
 
 
